3e14805723ea3e90a0cf9a5c5638aa2fd0e5814d
HAMZA ASIF <hamzaasifbaig190@gmail.com>
2024-07-12 14:56:16 +0500

    Day-9

--- frontend/src/pages/footer/about.jsx
+++ frontend/src/pages/footer/about.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const about = () => {
   return( 
-  <div>about</div>
+  <div>
+    <h1>About Boxing</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non neque felis. Donec tincidunt, nunc non condimentum ullamcorper, justo velit tempus lectus, et tristique odio sem nec neque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec vitae velit euismod, bibendum velit vel, gravida justo. Nulla facilisi. Sed efficitur, justo vel consectetur placerat, felis velit lobortis ligula, non vestibulum turpis neque vel lectus.</p>
+  </div>
   )
 }
 
