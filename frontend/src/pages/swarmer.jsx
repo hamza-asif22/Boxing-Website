7d5d9a81cefd0940eb145c74fb9195f01620558e
HAMZA ASIF <hamzaasifbaig190@gmail.com>
2024-07-17 15:09:57 +0500

    Day-10

--- frontend/src/pages/swarmer.jsx
+++ frontend/src/pages/swarmer.jsx
@@ -56,7 +56,7 @@ const Swarmer = () => {
               the left or right to cut off escape, and shifting back to throw
               off counters. The close-in fighter requires solid footwork (and
               top-level stamina) to move inside the reach of an opponent,
-              deliver a series of blows, and step out of danger. The Pressure
+              deliver a series of blows, and step out of danger.<br /><br /> The Pressure
               Fighter also has to be more than just comfortable with the clinch.
               This fighter has to know how to use the clinch tactically to
               affect the flow of the match. The clinch can be used to move an
@@ -86,22 +86,22 @@ const Swarmer = () => {
               from a distance and then dancing away. Because the Swarmer seeks
               to get inside an opponent’s range, they can eliminate this
               advantage. At close-range, a taller boxer can’t punch as
-              effectively, and may also have difficulty defending. Control the
+              effectively, and may also have difficulty defending.<br /><br /> Control the
               Fight – More than any other style, the Pressure style focuses on
               creating openings, rather than waiting for openings to appear. A
               Swarmer’s aggressive style also tends to force opponents into a
               reaction mindset. Rather than operate according to their own
               rhythm and strategy, a Swarmer’s opponent may end up on the
               defensive, constantly looking for opportunities to escape to a
-              safer distance. Additionally, the Pressure Fighter knows how to
+              safer distance.<br /><br /> Additionally, the Pressure Fighter knows how to
               use the clinch to control the fight. The Swarmer will enter a
               clinch to alter the speed and rhythm of an opponent. They might
               also clinch to physically alter the positions of both fighters
-              relative to each other and the ring. Overwhelm Defensive & Outside
+              relative to each other and the ring.<br /><br /> Overwhelm Defensive & Outside
               Fighters – Defensive fighters and fighters that prefer to fight
               outside can be overwhelmed by this style. A defensive boxer may
               face difficulty setting up counters if they are under a constant
-              barrage (or pinned down against the ropes). Likewise, an outside
+              barrage (or pinned down against the ropes).<br /><br /> Likewise, an outside
               fighter’s tactical advantage disappears when an opponent gets
               inside their range.
             </p>
@@ -127,7 +127,7 @@ const Swarmer = () => {
             <p className="col-span-3 text-2xl text-red-300 mt-4">
               While this tactic may be simple, it is not easy if you haven’t
               specifically trained yourself over the years to use the Brawler
-              style. If you aren’t a brawler, then learn to use the clinch to
+              style. <br /><br />If you aren’t a brawler, then learn to use the clinch to
               your advantage. First, you need to push back against a Swarmer’s
               desire to control ring position. Lower your center of gravity and
               set your feet to resist your opponent’s efforts to move you
@@ -135,13 +135,13 @@ const Swarmer = () => {
               them carry your weight. This will force them to manage a heavy
               load while also trying to move you around or attack you. Over
               time, you will wear your close-in opponent out (and save your legs
-              for later rounds). Speed and footwork will also provide some of
+              for later rounds). <br /><br />Speed and footwork will also provide some of
               your best advantages here. Use your speed to maintain distance.
               Use your footwork to keep moving laterally, rather than just
-              giving up ground by moving back. Finally, use your fists to punish
+              giving up ground by moving back. <br /><br />Finally, use your fists to punish
               a pressure fighter who wants to enter into your range. Don’t let
               them get close without making them fight their way in. Don’t punch
-              hard, but do punch often. The Swarmer fighting style has a lot of
+              hard, but do punch often. <br /><br />The Swarmer fighting style has a lot of
               advantages. It’s a great style to overcome size and speed
               disadvantages. It’s also aggressive. But it can be defeated if
               countered properly.
