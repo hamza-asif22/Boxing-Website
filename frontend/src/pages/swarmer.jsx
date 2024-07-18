e985c145f9c396c9a47b3117ea2cfda4c3c1a650
HAMZA ASIF <hamzaasifbaig190@gmail.com>
2024-07-18 10:59:40 +0500

    Day-11

--- frontend/src/pages/swarmer.jsx
+++ frontend/src/pages/swarmer.jsx
@@ -1,13 +1,40 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Swarmer = () => {
   return (
     <div
-      className="min-h-screen font-thin flex items-center justify-center text-white bg-cover bg-no-repeat bg-center"
-      style={{ backgroundImage: "url('/media/b7.jpg')" }}
+      className="min-h-screen bg-teal-500 font-thin flex items-center justify-center text-black bg-cover bg-no-repeat bg-center"
+      style={{ backgroundImage: `url('/media/a5.jpg')`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}
     >
-      <div className="max-w-4xl mx-auto p-4">
-        <h1 className="text-center font-serif font-bold italic text-2xl py-4">
+              <header className="absolute top-0 left-0 w-full z-20">
+          <div className=" mx-auto flex items-center justify-between py-4">
+            <h1 className="text-2xl font-semibold text-teal-800 hover:text-green-700 px-4">
+              Boxers Hub
+            </h1>
+            <div className="flex-1 flex justify-end space-x-10 pr-5">
+              <Link
+                to="/login"
+                className="text-lg font-semibold text-teal-800 hover:text-green-700"
+              >
+                Profile
+              </Link>
+              <Link
+                to="/pricing"
+                className="text-lg font-semibold text-teal-800 hover:text-green-700"
+              >
+                Pricing
+              </Link>
+              <Link
+                to="/contact"
+                className="text-lg font-semibold text-teal-800 hover:text-green-700"
+              >
+                Contact Us
+              </Link>
+            </div>
+          </div>
+        </header>
+      <div className="max-w-4xl mx-auto py-4">
+        <h1 className="text-center font-serif font-bold italic text-2xl py-8">
           Swarmer Boxing Style
         </h1>
         <div className="grid lg:grid-cols-3 gap-8 mb-8">
@@ -16,14 +43,14 @@ const Swarmer = () => {
             className="w-full col-span-1"
             alt="Swarmer Image"
           />
-          <p className="text-center text-2xl lg:text-left text-red-300 col-span-2">
+          <p className="text-center text-2xl lg:text-left text-black col-span-2">
             A fighter should enter the ring with an overall strategy and
             specific tactics to make that strategy happen. How you box in the
             ring, though, doesn’t just begin from a blank slate. You have to
             understand fighting styles, the kind that you use and the kind that
             your opponent will use.
           </p>
-          <p className="col-span-3 text-2xl text-red-300 mt-4">
+          <p className="col-span-3 text-2xl text-black mt-4">
             The Swarmer may be the most common fighting style out there. If done
             well, it can overcome a lot of physical disadvantages like reach and
             speed. The style also leads to a lot of punches being thrown and a
@@ -42,13 +69,13 @@ const Swarmer = () => {
               alt="Swarmer Image"
             />
 
-            <p className="text-center text-2xl lg:text-left text-red-300 col-span-2">
+            <p className="text-center text-2xl lg:text-left text-black col-span-2">
               The Swarmer, also known as the Pressure Fighter or the Close-In
               Fighter, prefers to get in close to their opponent and then attack
               with a flurry of blows. They may not hit hard, but they will tend
               to hit fast and often once inside.
             </p>
-            <p className="col-span-3 text-2xl text-red-300 mt-4">
+            <p className="col-span-3 text-2xl text-black mt-4">
               This type of fighter will also tend to throw a variety of punches
               in order to circumvent an opponent’s defenses. This style places a
               premium on good footwork. A Close-In fighter wants to place
@@ -75,13 +102,13 @@ const Swarmer = () => {
               alt="Swarmer"
             />
 
-            <p className="text-center text-2xl lg:text-left text-red-300 col-span-2">
+            <p className="text-center text-2xl lg:text-left text-black col-span-2">
               There are a number of reasons that a fighter might choose the
               close-in style. Negate Height Advantages – Shorter boxers often
               choose this style because it can negate the reach advantage of
               taller, longer-limbed opponents.
             </p>
-            <p className="col-span-3 text-2xl text-red-300 mt-4">
+            <p className="col-span-3 text-2xl text-black mt-4">
               Tall fighters often choose to keep opponents at range, striking
               from a distance and then dancing away. Because the Swarmer seeks
               to get inside an opponent’s range, they can eliminate this
@@ -117,14 +144,14 @@ const Swarmer = () => {
               alt="Swarmer"
             />
 
-            <p className="text-center text-2xl lg:text-left text-red-300 col-span-2">
+            <p className="text-center text-2xl lg:text-left text-black col-span-2">
               The simplest way to defend against a Swarmer is to be a
               Brawler-style fighter. A brawler relies on power and durability to
               take out opponents. They invite their opponents to get close,
               weather the storm, and then smash their opponents with powerful
               strikes.
             </p>
-            <p className="col-span-3 text-2xl text-red-300 mt-4">
+            <p className="col-span-3 text-2xl text-black mt-4">
               While this tactic may be simple, it is not easy if you haven’t
               specifically trained yourself over the years to use the Brawler
               style. <br /><br />If you aren’t a brawler, then learn to use the clinch to
@@ -144,10 +171,19 @@ const Swarmer = () => {
               hard, but do punch often. <br /><br />The Swarmer fighting style has a lot of
               advantages. It’s a great style to overcome size and speed
               disadvantages. It’s also aggressive. But it can be defeated if
-              countered properly.
+              countered properly.<br/><br/><br/>
             </p>
           </div>
+          <h1>--------------------------------------------------------------------------------------------------------------------------------------------</h1>
+          <br/><br/>
+          <h1 className="flex text-center font-normal">Start your training now by becoming our member and get access to our exclusive content and live sessions and many more. Visit our Pricing plan and start your journey to become a profesional boxer.
+            
+            </h1>
+          <h1 className="font-sans hover:bg-blue-300 border  border-blue-800 rounded-xl text-pretty font-semibold text-xl flex justify-center text-center">For More Details and real life experience and training get our 
+            premium membership. Download our app 'Boxers Hub' to get mastered in
+            different boxing styles. <br /> In case of any query feel free to contact us.</h1>
         </div>
+        
       </div>
     </div>
   );
