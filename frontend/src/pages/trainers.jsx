e6c6be5b122cae91d752ff88dbe2ed3e0e2fdec2
HAMZA ASIF <hamzaasifbaig190@gmail.com>
2024-08-15 12:52:03 +0500

    Day-16

--- frontend/src/pages/trainers.jsx
+++ frontend/src/pages/trainers.jsx
@@ -1,63 +1,134 @@
 import React from 'react';
-import { FaFacebook, FaTwitter, FaLinkedin } from 'react-icons/fa';
 
-const teamMembers = [
-  {
-    name: 'Samantha Sarah',
-    position: 'Founder & CEO',
-    imgSrc: 'https://bootdey.com/img/Content/avatar/avatar1.png',
-  },
-  {
-    name: 'Nazrul Islam',
-    position: 'UI Designer',
-    imgSrc: 'https://bootdey.com/img/Content/avatar/avatar7.png',
-  },
-  {
-    name: 'Riyadh Khan',
-    position: 'Developer',
-    imgSrc: 'https://bootdey.com/img/Content/avatar/avatar6.png',
-  },
-  {
-    name: 'Niloy Islam',
-    position: 'Marketing Manager',
-    imgSrc: 'https://bootdey.com/img/Content/avatar/avatar2.png',
-  },
-];
+const Trainers = () => {
+  const trainersData = [
+    {
+      name: 'Hamza Asif Baig',
+      role: 'Founder/CEO',
+      website: ' RingWarriors.pk',
+      image: 'https://bootdey.com/img/Content/avatar/avatar2.png',
+      walletBalance: 2563,
+      incomeAmount: 6952,
+      transactions: 1125,
+      socialLinks: {
+        facebook: '#',
+        twitter: '#',
+        skype: '#',
+      },
+    },
+    {
+      name: 'Julie L. Arsenault',
+      role: '@Programmer',
+      website: 'websitename.com',
+      image: 'https://bootdey.com/img/Content/avatar/avatar3.png',
+      walletBalance: 8471,
+      incomeAmount: 8512,
+      transactions: 4751,
+      socialLinks: {
+        facebook: '#',
+        twitter: '#',
+        skype: '#',
+      },
+    },
+    {
+      name: 'Julie L. Arsenault',
+      role: '@Programmer',
+      website: 'websitename.com',
+      image: 'https://bootdey.com/img/Content/avatar/avatar3.png',
+      walletBalance: 8471,
+      incomeAmount: 8512,
+      transactions: 4751,
+      socialLinks: {
+        facebook: '#',
+        twitter: '#',
+        skype: '#',
+      },
+    },
+    
+  ];
 
-const TeamSection = () => {
   return (
-    <div className="container mx-auto px-4 py-12 bg-[#eee]">
-      <div className="text-center mb-12">
-        <h3 className="text-3xl font-bold mb-4">
-          Our Creative <span className="text-blue-600">Team</span>
-        </h3>
-        <p className="text-gray-600">
-          Appland is completely creative, lightweight, clean & super responsive app landing page.
-        </p>
-        <div className="w-16 mx-auto border-b-2 border-blue-600 my-4"></div>
-      </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-        {teamMembers.map((member, index) => (
-          <div key={index} className="relative bg-white rounded-lg overflow-hidden shadow-lg mb-12 transition-transform transform hover:scale-105">
-            <div className="relative bg-blue-600 p-8 rounded-t-lg overflow-hidden">
-              <img src={member.imgSrc} alt={member.name} className="w-full h-48 object-cover rounded-t-lg" />
-              <div className="absolute bottom-0 right-4 text-black flex space-x-4 z-10 group">
-                <a href="#" className="text-lg hover:text-white"><FaFacebook /></a>
-                <a href="#" className="text-lg hover:text-white"><FaTwitter /></a>
-                <a href="#" className="text-lg hover:text-white"><FaLinkedin /></a>
-              </div>
-              <div className="absolute bottom-[-45px] left-[-25%] w-[150%] h-20 bg-white transform rotate-[-15deg] z-0"></div>
-            </div>
-            <div className="p-8 text-right bg-white rounded-b-lg relative">
-              <div className="absolute top-3 right-8 w-12 h-1 bg-blue-600 z-10"></div>
-              <h6 className="text-xl font-semibold mb-1 text-gray-900">{member.name}</h6>
-              <p className="text-gray-600">{member.position}</p>
-            </div>
+    <div className="content">
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.3/css/all.min.css"
+      />
+      <div className="container mx-auto px-4">
+        <div className="flex flex-wrap">
+          <div className="w-full sm:w-1/3 mb-4">
+            <a
+              href="#custom-modal"
+              className="btn-custom mb-4 inline-flex items-center px-4 py-2 text-white bg-blue-500 hover:bg-blue-600 rounded shadow"
+            >
+              <i className="mdi mdi-plus"></i> Add Member
+            </a>
           </div>
-        ))}
+        </div>
+
+        <div className="flex flex-wrap">
+          {trainersData.map((trainer, index) => (
+            <div key={index} className="w-full lg:w-1/3 px-4 mb-4">
+              <div className="card-box bg-white rounded shadow text-center p-6">
+                <div className="member-card">
+                  <div className="member-thumb mx-auto w-24 h-24 mb-4">
+                    <img
+                      src={trainer.image}
+                      className="rounded-full shadow"
+                      alt="profile-image"
+                    />
+                  </div>
+                  <h4 className="text-xl font-semibold">{trainer.name}</h4>
+                  <p className="text-gray-500">
+                    {trainer.role} <span>|</span>
+                    <a href="#" className="text-pink-500">{trainer.website}</a>
+                  </p>
+                  <ul className="social-links list-inline flex justify-center mt-4">
+                    <li className="list-inline-item mx-2">
+                      <a href={trainer.socialLinks.facebook} title="Facebook" className="text-blue-600">
+                        <i className="fab fa-facebook"></i>
+                      </a>
+                    </li>
+                    <li className="list-inline-item mx-2">
+                      <a href={trainer.socialLinks.twitter} title="Twitter" className="text-blue-400">
+                        <i className="fab fa-twitter"></i>
+                      </a>
+                    </li>
+                    <li className="list-inline-item mx-2">
+                      <a href={trainer.socialLinks.skype} title="Skype" className="text-blue-600">
+                        <i className="fab fa-skype"></i>
+                      </a>
+                    </li>
+                  </ul>
+                  <button
+                    type="button"
+                    className="mt-4 btn btn-primary bg-blue-500 text-white px-4 py-2 rounded-full"
+                  >
+                    Message Now
+                  </button>
+                  <div className="mt-6">
+                    <div className="grid grid-cols-3 gap-4">
+                      <div>
+                        <h4 className="text-2xl font-bold">{trainer.walletBalance}</h4>
+                        <p className="text-gray-500">Wallets Balance</p>
+                      </div>
+                      <div>
+                        <h4 className="text-2xl font-bold">{trainer.incomeAmount}</h4>
+                        <p className="text-gray-500">Income amounts</p>
+                      </div>
+                      <div>
+                        <h4 className="text-2xl font-bold">{trainer.transactions}</h4>
+                        <p className="text-gray-500">Total Transactions</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
 };
 
-export default TeamSection;
+export default Trainers;
