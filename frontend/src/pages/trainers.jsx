ba71892934222e09708dfdbb070debf49a981c96
HAMZA ASIF <hamzaasifbaig190@gmail.com>
2024-08-15 13:51:48 +0500

    Day-17

--- frontend/src/pages/trainers.jsx
+++ frontend/src/pages/trainers.jsx
@@ -1,50 +1,49 @@
-import React from 'react';
+import React from "react";
 
 const Trainers = () => {
   const trainersData = [
     {
-      name: 'Hamza Asif Baig',
-      role: 'Founder/CEO',
-      website: ' RingWarriors.pk',
-      image: 'https://bootdey.com/img/Content/avatar/avatar2.png',
-      walletBalance: 2563,
-      incomeAmount: 6952,
-      transactions: 1125,
+      name: "Mike Tyson",
+      role: "Senior Trainer/Coach",
+      website: " RingWarriors.pk",
+      image: "https://bootdey.com/img/Content/avatar/avatar2.png",
+      totalfights: 58,
+      KO: 44,
+      record: "50-6",
       socialLinks: {
-        facebook: '#',
-        twitter: '#',
-        skype: '#',
+        facebook: "#",
+        twitter: "#",
+        instagram: "#",
       },
     },
     {
-      name: 'Julie L. Arsenault',
-      role: '@Programmer',
-      website: 'websitename.com',
-      image: 'https://bootdey.com/img/Content/avatar/avatar3.png',
-      walletBalance: 8471,
-      incomeAmount: 8512,
-      transactions: 4751,
+      name: "Conor Mcgregor",
+      role: "Senior Trainer/Coach",
+      website: " RingWarriors.pk",
+      image: "https://bootdey.com/img/Content/avatar/avatar2.png",
+      totalfights: 28,
+      KO: 19,
+      record: "22-6-0",
       socialLinks: {
-        facebook: '#',
-        twitter: '#',
-        skype: '#',
+        facebook: "#",
+        twitter: "#",
+        instagram: "#",
       },
     },
     {
-      name: 'Julie L. Arsenault',
-      role: '@Programmer',
-      website: 'websitename.com',
-      image: 'https://bootdey.com/img/Content/avatar/avatar3.png',
-      walletBalance: 8471,
-      incomeAmount: 8512,
-      transactions: 4751,
+      name: "Mike Tyson",
+      role: "Senior Trainer",
+      website: " RingWarriors.pk",
+      image: "https://bootdey.com/img/Content/avatar/avatar2.png",
+      totalfights: 58,
+      KO: 44,
+      record: "50-6",
       socialLinks: {
-        facebook: '#',
-        twitter: '#',
-        skype: '#',
+        facebook: "#",
+        twitter: "#",
+        instagram: "#",
       },
     },
-    
   ];
 
   return (
@@ -80,44 +79,59 @@ const Trainers = () => {
                   <h4 className="text-xl font-semibold">{trainer.name}</h4>
                   <p className="text-gray-500">
                     {trainer.role} <span>|</span>
-                    <a href="#" className="text-pink-500">{trainer.website}</a>
+                    <a href="#" className="text-pink-500">
+                      {trainer.website}
+                    </a>
                   </p>
                   <ul className="social-links list-inline flex justify-center mt-4">
-                    <li className="list-inline-item mx-2">
-                      <a href={trainer.socialLinks.facebook} title="Facebook" className="text-blue-600">
+                  <li className="list-inline-item mx-2">
+                      <a
+                        href={trainer.socialLinks.facebook}
+                        title="FaceBook"
+                        className="text-blue-600"
+                      >
                         <i className="fab fa-facebook"></i>
                       </a>
                     </li>
                     <li className="list-inline-item mx-2">
-                      <a href={trainer.socialLinks.twitter} title="Twitter" className="text-blue-400">
-                        <i className="fab fa-twitter"></i>
+                      <a
+                        href={trainer.socialLinks.Instagram}
+                        title="Instagram"
+                        className="text-blue-600"
+                      >
+                        <i className="fab fa-instagram"></i>
                       </a>
                     </li>
                     <li className="list-inline-item mx-2">
-                      <a href={trainer.socialLinks.skype} title="Skype" className="text-blue-600">
-                        <i className="fab fa-skype"></i>
+                      <a
+                        href={trainer.socialLinks.twitter}
+                        title="Twitter"
+                        className="text-blue-600"
+                      >
+                        <i className="fab fa-twitter"></i>
                       </a>
                     </li>
                   </ul>
-                  <button
-                    type="button"
-                    className="mt-4 btn btn-primary bg-blue-500 text-white px-4 py-2 rounded-full"
-                  >
-                    Message Now
-                  </button>
+                 
                   <div className="mt-6">
                     <div className="grid grid-cols-3 gap-4">
                       <div>
-                        <h4 className="text-2xl font-bold">{trainer.walletBalance}</h4>
-                        <p className="text-gray-500">Wallets Balance</p>
+                        <h4 className="text-2xl font-bold">
+                          {trainer.totalfights}
+                        </h4>
+                        <p className="text-gray-500">Total Fights</p>
                       </div>
                       <div>
-                        <h4 className="text-2xl font-bold">{trainer.incomeAmount}</h4>
-                        <p className="text-gray-500">Income amounts</p>
+                        <h4 className="text-2xl font-bold">
+                          {trainer.KO}
+                        </h4>
+                        <p className="text-gray-500">KO's</p>
                       </div>
                       <div>
-                        <h4 className="text-2xl font-bold">{trainer.transactions}</h4>
-                        <p className="text-gray-500">Total Transactions</p>
+                        <h4 className="text-2xl font-bold">
+                          {trainer.record}
+                        </h4>
+                        <p className="text-gray-500">Records</p>
                       </div>
                     </div>
                   </div>
